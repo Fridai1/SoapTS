5a4a9d9eb5c7d26d65f79d33f9784243257c3ef8
Fridai <Nikolaiviby3@gmail.com>
2018-03-05 15:41:51 +0100

    lavet unit test samt lavet ctor

--- SoapTS/Service1.svc.cs
+++ SoapTS/Service1.svc.cs
@@ -13,6 +13,10 @@ namespace SoapTS
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
+        public Service1()
+        {
+            
+        }
         public string GetName()
         {
             Class1 s = new Class1();
---
+++ SoapTSTests/Service1Tests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoapTS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapTS.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        private Service1 s;
+
+        public Service1Tests()
+        {
+            s = new Service1();
+        }
+
+        [TestMethod()]
+        public void GetNameTest()
+        {
+            Assert.AreEqual("Nikolai", s.GetName());
+        }
+    }
+}
